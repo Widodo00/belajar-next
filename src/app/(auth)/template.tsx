264d462baa1ea4a9963ff0064cc02ea25c11d922
04widodo <77629007+Widodo00@users.noreply.github.com>
2023-12-27 11:37:00 +0700

    complex-dashboard

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navLinks = [
   { name: "Register", href: "/register" },
@@ -10,8 +11,13 @@ const navLinks = [
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const [input, setInput] = useState("");
+
   return (
     <div>
+      <div>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
       {navLinks.map((link) => {
         const isActive = pathname.startsWith(link.href);
         return (
